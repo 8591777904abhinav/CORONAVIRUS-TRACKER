d800e7b4a22ce4de1ab892387a3fda8b3f1c460c
8591777904abhinav <63181611+8591777904abhinav@users.noreply.github.com>
2020-09-02 16:39:47 +0530

    Updates

---
+++ precache-manifest.9aebf63ae3283450695b5deb258403d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efdb58131e61a6255cc9df8db9c442f8",
+    "url": "/8591777904abhinav/CORONAVIRUS-TRACKER/index.html"
+  },
+  {
+    "revision": "d5028022144f6edac520",
+    "url": "/8591777904abhinav/CORONAVIRUS-TRACKER/static/css/main.1b373a34.chunk.css"
+  },
+  {
+    "revision": "4f050f462e4177cd80ca",
+    "url": "/8591777904abhinav/CORONAVIRUS-TRACKER/static/js/2.3d349935.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/8591777904abhinav/CORONAVIRUS-TRACKER/static/js/2.3d349935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5028022144f6edac520",
+    "url": "/8591777904abhinav/CORONAVIRUS-TRACKER/static/js/main.2bb4a9fb.chunk.js"
+  },
+  {
+    "revision": "43dad89ed63c5d4c4023",
+    "url": "/8591777904abhinav/CORONAVIRUS-TRACKER/static/js/runtime-main.0ec778c2.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/8591777904abhinav/CORONAVIRUS-TRACKER/static/media/image.d7265326.png"
+  }
+]);
